1c17a1cda469dac93ae5d0c86b6c9a662695a178
Osmar Neto <88497805+OsmarBaia@users.noreply.github.com>
2024-06-10 19:15:19 -0300

    Tarefa terminada, adicionado collator para ajeitar ordenação

---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.text.Collator;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            //Entrada Manual
+            // System.out.println("Entre a lista de nomes, os nomes devem ser separados por virgulas");            
+            // String entriedNames = sc.nextLine();            
+
+
+            //Entrada Simulada
+            String entriedNames =  "Maria, João, Ana, Pedro, Sofia, Lucas, Laura, Miguel, Isabela, Gabriel";                   
+            
+            String[] names =  entriedNames.trim().split(",");
+            
+            Collator collator = Collator.getInstance(new Locale("pt", "BR"));
+            Arrays.sort(names,collator);
+            
+            for (String nome : names) {
+            System.out.println(nome);
+            }
+        }
+    }
+}
