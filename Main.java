ac9d9e67ce983edcb2bbd797c72388d1500d1d25
Osmar Neto <88497805+OsmarBaia@users.noreply.github.com>
2024-06-10 15:53:02 -0300

    Criação do main e de uma classe simples

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+       Square sq1 =  new Square(2);
+       System.out.printf("A area do quadrado é: %.2f", sq1.getArea());
+    }
+}
+
