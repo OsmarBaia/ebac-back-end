f992ff62b1687c10d7e83ff63b08c48a42b4a362
Osmar Baia <osmar.neto23@gmail.com>
2024-06-12 11:45:25 -0300

    Atividade com herança

--- PessoaFisica.java
+++ PessoaFisica.java
@@ -1,2 +1,27 @@
 public class PessoaFisica extends Pessoa{
+    private String cpf;
+
+    // Construtor
+    public PessoaFisica(String nome, String endereco, String cpf) {
+        super(nome, endereco);
+        this.cpf = cpf;
+    }
+
+    // Getter e Setter específico
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void pagarImpostos() {
+        System.out.println(getNome() + " está pagando impostos como Pessoa Física.");
+    }
+
+    @Override
+    public void apresentar() {
+        System.out.println("Olá, eu sou " + getNome() + " e sou uma Pessoa Física.");
+    }
 }
--- PessoaJuridica.java
+++ PessoaJuridica.java
@@ -1,2 +1,29 @@
 public class PessoaJuridica extends Pessoa{
+    private String cnpj;
+
+    // Construtor
+    public PessoaJuridica(String nome, String endereco, String cnpj) {
+        super(nome, endereco);
+        this.cnpj = cnpj;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public void pagarTributos() {
+        System.out.println(getNome() + " está pagando tributos como Pessoa Jurídica.");
+    }
+
+    // Implementação do método polimórfico
+    @Override
+    public void apresentar() {
+        System.out.println("Olá, nós somos a empresa " + getNome() + " e somos uma Pessoa Jurídica.");
+    }
+
 }
+
